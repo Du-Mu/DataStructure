ab399e77875cab1f61ef73cfa90be8ee3763baeb
Nemo <wzhdxtx123@outlook.com>
2022-02-28 22:31:08 +0800

    creat and output graph

--- Graph/graph2.c
+++ Graph/graph2.c
@@ -1,3 +1,6 @@
+#include <stdio.h>
+
+#define INC 32767
 #define MAXV 100
 typedef struct ANode
 {
@@ -16,3 +19,42 @@ typedef struct
     int n, e;               //the num of vertices and edges
 }AdjGraph;                  //the entire Graph
 
+void CreateAdj(AdjGraph *G, int A[MAXV][MAXV], int n, int e)
+{//creat a graph
+    int j;
+    ArcNode *p;
+    G = (AdjGraph *)malloc(sizeof(AdjGraph));
+    
+    for (int i = 0 ; i < n ; i++)
+        (G->adijist[i].firstarc) = NULL;
+
+    for (int i = 0 ; i < n ; i++)
+        for (int j = n-1 ; j >= 0 ; j--)
+            if (A[i][j]!=0 && A[i][j]!=INC)
+            {
+                p = (ArcNode *)malloc(sizeof(ArcNode));
+                p->adjvex = j;
+                p->weigh = A[i][j];
+                p->nextarc = G->adijist[i].firstarc;
+                G->adijist[i].firstarc = p;
+            }
+
+    G->n = n;
+    G->e = e;
+}
+void DispAdj(AdjGraph *G)
+{//output the adjacency lisy
+    ArcNode *p;
+    for (int i = 0 ; i < G->n ; i++)
+    {
+        p = G->adijist[i].firstarc;
+        print("%3d:",i);
+        while (p!=NULL)
+        {
+            printf("%3d[%d]", p->adjvex, p->weigh);
+            p = p->nextarc;
+        }
+        printf("^\n");
+    }
+}
+ 
