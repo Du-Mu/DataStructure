203eb6d44c8abf0ee054335fc50ce49b3316cb88
Nemo <wzhdxtx123@outlook.com>
2022-02-28 20:40:09 +0800

    graph

--- Graph/graph2.c
+++ Graph/graph2.c
@@ -1,6 +1,18 @@
+#define MAXV 100
 typedef struct ANode
 {
-    int adjvex;//the adjacent point number of the edge
-    struct ANode * nextarc;//the pointer to the next edge
-    int weigh
-};
+    int adjvex;             //the adjacent point number of the edge
+    struct ANode *nextarc;  //the pointer to the next edge
+    int weigh;              //the weigh of every nodes
+}ArcNode;                   //edge nodes
+typedef struct
+{
+    int value;              //the other information 
+    ArcNode *firstarc;      //point to the first edge nodes
+}VNode;                     //header type
+typedef struct 
+{
+    VNode adijist[MAXV];    //the header array
+    int n, e;               //the num of vertices and edges
+}AdjGraph;                  //the entire Graph
+
